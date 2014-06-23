b6eedb1b7c61d50e8b22e554df3caa24fd7c5a09
jsassoon <jsassoon@jsassoon-macbookpro4.roam.corp.google.com>
2014-06-22 23:12:44 -0700

    creating repository on github
    creating repository on github

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Josh Sassoon on 6/17/14.
+//  Copyright (c) 2014 youtubeux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Josh Sassoon on 6/17/14.
+//  Copyright (c) 2014 youtubeux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
